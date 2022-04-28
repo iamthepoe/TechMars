06db3980ac88b372c2d521f7a574084bf303d710
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-04-28 09:36:55 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+document.getElementById('logo').addEventListener('mouseover', ()=>{
+    let logo = document.getElementById('logo');
+    logo.style.marginLeft = "200px";
+});
+
+document.getElementById('logo').addEventListener('mouseover', ()=>{
+    let logo = document.getElementById('logo');
+    logo.style.marginLeft = "0px";
+});
