b14981e301cbff37f54317f720c8db53ad1b9c86
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-04-29 00:09:16 -0300

    Add files via upload

---
+++ assets/vocation.js
@@ -0,0 +1,113 @@
+let question = document.querySelector('.main-grid-question');
+let answer = document.querySelectorAll('.main-grid-answer');
+let perguntas = [
+	"Tenho mais afinidade com lógica do que com artes.",
+	"Tenho perfil de líder/empreendedor.",
+	"Sou uma pessoa imaginativa.",
+	"Gosto de lidar com pessoas.",
+	"Mantenho constante aprendizado em minhas atividades."
+];
+let des = 0; let jodi = 0; let mad = 0; let q = 0;
+let verificar = (()=>{
+	if(q==5){
+		if(des>jodi && des>mad){
+			question.innerHTML = "Desenvolvimento de Sistemas";
+		}else if(jodi>des && jodi>mad){
+			question.innerHTML = "Desenvolvimento de Jogos Digitais";
+		}else{
+			question.innerHTML = "Administração";
+		}
+		q = 0;
+		jodi = 0;
+		mad = 0;
+		des = 0;
+	}
+});
+
+answer[0].addEventListener('click', ()=>{
+	question.innerHTML = perguntas[q];
+	switch(q){
+		case 1:
+			des+= 2;
+			mad+= 1;
+		break;
+
+		case 2:
+			mad+= 2;
+		break;
+
+		case 3:
+			jodi+=2;
+			des+=1;
+		break;
+
+		case 4:
+			mad+=2;
+			des-=2;
+			jodi-=1;
+		break;
+
+		case 5:
+			des++;
+			jodi++;
+			mad++;
+		break;
+	}
+	verificar();
+	q++;
+});
+
+answer[1].addEventListener('click', ()=>{
+	question.innerHTML = perguntas[q];
+	switch(q){
+		case 1:
+			jodi+=1;
+		break;
+
+		case 2:
+		break;
+
+		case 3:
+		break;
+
+		case 4:
+		break;
+
+		case 5:
+		break;
+	}	
+	verificar();
+	q++;
+});
+
+answer[2].addEventListener('click', ()=>{
+	question.innerHTML = perguntas[q];
+	switch(q){
+		case 1:
+			jodi+=2;
+		break;
+
+		case 2:
+			jodi++;
+			mad--;
+		break;
+
+		case 3:
+			mad--;
+		break;
+
+		case 4:
+			jodi+=2;
+			des+=2;
+			mad--;
+		break;
+
+		case 5:
+			des--;
+			jodi--;
+			mad--;
+		break;
+	}
+	verificar();
+	q++;
+});
